6a7075c5bfe0037ba0f9a1166baca1cf50307d62
sid <73837135+Gargantuan5k@users.noreply.github.com>
2025-04-08 22:54:20 +0530

    Fix I2C read/write, I2C device address, datatypes for x y z
    Code now works 🙏🙏🙏

--- magfield.c
+++ magfield.c
@@ -7,7 +7,12 @@
 #define QMC5883L_ADDRESS 0x0D
 #define CONTROL_REG1 0x09 // operational modes (MODE). output data update rate (ODR), magnetic field measurement range or sensitivity of the sensors (RNG) and over sampling rate (OSR)
 #define CONTROL_REG2 0x0A // Interrupt Pin enabling (INT_ENB), Point roll over function enabling(POL_PNT) and soft reset (SOFT_RST)
-#define DATA_REGISTER 0x00 // read magnetic field values 
+#define X_LSB 0x00 // read magnetic field values 
+#define X_MSB 0x01
+#define Y_LSB 0x02
+#define Y_MSB 0x03
+#define Z_LSB 0x04
+#define Z_MSB 0x05
 #define STATUS_REGISTER 0x06
 #define SET_RESET_PERIOD_REGISTER 0x0b // recommended as per datasheet
 
@@ -17,37 +22,74 @@ void writeRegister(int file, unsigned char reg, unsigned char value)
     write(file, buffer, 2);
 }
 
-void readMagneticField(int file, int *x, int *y, int *z)
+
+void readMagneticField(int file, __int16_t *x, __int16_t *y, __int16_t *z)
 {
-    unsigned char data_buffer[6];
-    unsigned char status_buf[6];
+    unsigned char x_lsb[1];
+    x_lsb[0] = X_LSB;
+
+    unsigned char x_msb[1];
+    x_msb[0] = X_MSB;
+
+    unsigned char y_lsb[1];
+    y_lsb[0] = Y_LSB;
+
+    unsigned char y_msb[1];
+    y_msb[0] = Y_MSB;
+
+    unsigned char z_lsb[1];
+    z_lsb[0] = Z_LSB;
+
+    unsigned char z_msb[1];
+    z_msb[0] = Z_MSB;
+    // unsigned char status_buf[6];
 
     // Read from STATUS_REGISTER to ensure we're ready
-    write(file, STATUS_REGISTER, 1);
-    read(file, status_buf, 6);
-    if (status_buf[0]) // 0x06[0] = 1 means ready
-    {
-        write(file, DATA_REGISTER, 1); // now read from data reg
-        read(file, data_buffer, 6);
-    }
+    // write(file,  1);
+    // read(file, status_buf, 6);
+    // if (status_buf[0]) // 0x06[0] = 1 means ready
+    // {
+        // write(file, DATA_REGISTER, 1); // now read from data reg
+        
+        // read(file, x_lsb, 2);
 
-    *x = (data_buffer[1] << 8) | data_buffer[0]; // shift high byte left by 8b then cat with low byte
-    *y = (data_buffer[3] << 8) | data_buffer[2];
-    *z = (data_buffer[5] << 8) | data_buffer[4];
+    // }
+
+    write(file, x_lsb, 1);
+    read(file, x_lsb, 1);
+
+    write(file, x_msb, 1);
+    read(file, x_msb, 1);
+
+    write(file, y_lsb, 1);
+    read(file, y_lsb, 1);
+
+    write(file, y_msb, 1);
+    read(file, y_msb, 1);
+
+    write(file, z_lsb, 1);
+    read(file, z_lsb, 1);
+
+    write(file, z_msb, 1);
+    read(file, z_msb, 1);
+
+    *x = (__int16_t)((x_msb[0] << 8) | x_lsb[0]); // shift high byte left by 8b then cat with low byte
+    *y = (__int16_t)((y_msb[0] << 8) | y_lsb[0]);
+    *z = (__int16_t)((z_msb[0] << 8) | z_lsb[0]);
 
     // Handle negative values (2s complement)
-    if (*x > 32767)
-        *x -= 65536;
-    if (*y > 32767)
-        *y -= 65536;
-    if (*z > 32767)
-        *z -= 65536;
+    if (*x < 0)
+        *x = ~(*x) + 1;
+    if (*y < 0)
+        *y = ~(*y) + 1;
+    if (*z < 0)
+        *z = ~(*z) + 1;
 }
 
 int main()
 {
     int file;
-    const char *filename = "/dev/i2c-1";
+    const char *filename = "/dev/i2c-2";
 
     // Open I2C bus
     if ((file = open(filename, O_RDWR)) < 0)
@@ -69,7 +111,7 @@ int main()
     writeRegister(file, CONTROL_REG2, 0x00); // Default settings
 
     // Read magnetic field values
-    int x, y, z;
+    __int16_t x, y, z;
     while (1)
     {
         readMagneticField(file, &x, &y, &z);
