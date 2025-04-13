9cdee1aefe34e1be6ea49f1e120b6153d6101c3a
sid <73837135+Gargantuan5k@users.noreply.github.com>
2025-04-13 22:11:53 +0530

    Add good comments

--- bbb_spi.c
+++ bbb_spi.c
@@ -7,20 +7,19 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define MODE SPI_MODE_0
-#define BITS 8
-#define CLK_SPD 500000
-// #define LEN 1
-#define DEVICE "/dev/spidev1.0"
+#define MODE SPI_MODE_0 // CPOL=0, CPHA=0
+#define BITS 8 // bits per word of communication
+#define CLK_SPD 500000 // 500 kHz freq.
+#define DEVICE "/dev/spidev1.0" // spi device interface on the bbb
 
 int main(void)
 {
-    uint8_t tx[32] = {69};
-    uint8_t rx[32] = {0};
+    uint8_t tx[32] = {69}; // probably doesn't need to be 32 bits??
+    uint8_t rx[32] = {0};  // probably doesn't need to be 32 bits??
     int fd;
-    uint32_t messenger;
+    uint32_t messenger; // stores the data for ioctl requests
 
-    struct spi_ioc_transfer trx_obj;
+    struct spi_ioc_transfer trx_obj; // spi packet (trx object)
 
     // setup spi trx object
     trx_obj.tx_buf = (unsigned long)tx;
@@ -39,23 +38,23 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    if ((ioctl(fd, SPI_IOC_RD_MODE32, &messenger)) == -1)
+    if ((ioctl(fd, SPI_IOC_RD_MODE32, &messenger)) == -1) // read mode
     {
         perror("spi mode read");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    messenger |= MODE;
+    messenger |= MODE; // add the MODE to messenger, will write next
 
-    if ((ioctl(fd, SPI_IOC_WR_MODE32, &messenger)) == -1)
+    if ((ioctl(fd, SPI_IOC_WR_MODE32, &messenger)) == -1) // write mode
     {
         perror("spi mode write");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    if ((ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &messenger)) == -1)
+    if ((ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &messenger)) == -1) // read frequency
     {
         perror("spi max speed read");
         close(fd);
@@ -64,14 +63,14 @@ int main(void)
 
     messenger = CLK_SPD;
 
-    if ((ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &messenger)) == -1)
+    if ((ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &messenger)) == -1) // write frequency
     {
         perror("spi max speed write");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    if ((ioctl(fd, SPI_IOC_MESSAGE(1), &trx_obj)) == -1)
+    if ((ioctl(fd, SPI_IOC_MESSAGE(1), &trx_obj)) == -1) // send spi message
     {
         perror("Nonzero return value during SPI comm");
         exit(EXIT_FAILURE);
